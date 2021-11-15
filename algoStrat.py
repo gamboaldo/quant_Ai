96c93d865f1fa36ca66f8de9c3d7792d05c8ba4b
Aldo Mata <gamboaldo@gmail.com>
2021-11-15 11:09:30 -0800

    loop

--- algoStrat.py
+++ algoStrat.py
@@ -22,3 +22,21 @@ plt.plot(data[f'SMA_{ma_1}'], label=f"SMA_{ma_1}", color="orange")
 plt.plot(data[f'SMA_{ma_2}'], label=f"SMA_{ma_2}", color="blue")
 plt.legend(loc="upper left")
 plt.show()
+
+buy_signals = []
+sell_signals = []
+trigger = 0
+
+# if ma_1 is larger than 2 and trigger not 1, we have buy signal at location x, leave sell_sig to empty, set trigger to 1, and vice verse
+for x in range(len(data)):
+    if data[f'SMA_{ma_1}'].iloc[x] > data[f'SMA_{ma_2}'].iloc[x] and trigger != 1:
+        buy_signals.append(data['Adj Close'].iloc[x])
+        sell_signals.append(float('nan'))
+        trigger = 1
+    elif data[f'SMA_{ma_1}'].iloc[x] < data[f'SMA_{ma_2}'].iloc[x] and trigger != -1:
+        buy_signals.append(float('nan'))
+        sell_signals.append(data['Adj Close'].iloc[x])
+        trigger = -1
+    else:
+        buy_signals.append(float('nan'))
+        sell_signals.append(float('nan'))
